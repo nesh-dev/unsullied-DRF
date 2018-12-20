623059ffab90a1b4f03c2b31014f7ea8eb77c672
Sylvia Wanjiku Mbugua <sylvia@Sylvias-MacBook-Pro.local>
2018-12-20 12:45:21 +0300

    [chore #162758465]Create tests for Get all and single tasks

--- todoList/tasks/tests.py
+++ todoList/tasks/tests.py
@@ -7,7 +7,11 @@ from rest_framework import status
 
 from rest_framework.test import APITestCase
 
+from rest_framework.test import APIClient
+
 from .models import Todo
+
+from .serializers import TodoSerializer
 
 
 class TodolistTest(APITestCase):
@@ -15,21 +19,68 @@ class TodolistTest(APITestCase):
     url = reverse('todos')
     def setUp(self):
         self.client = APIClient()
-        self.todo-data = {'title':'Watch Documentary'}
-        self.response =self.client.post(reverse('tasks'),self.todo-data,format="json")
+        self.data = {'title':'Watch Documentary'}
+        # self.response =self.client.post(reverse('todos'),self.data,format="json")
     
     def test_create_task(self):
         """
         Ensure a task can be created
         """
-        # response = client.post(/task/,{'title':'Eat'})
+        response =self.client.post(reverse('todos'),self.data,format="json")
         self.assertEqual(201,response.status_code)
 
     # def test_can_get_all_tasks(self):
     #     """Test if api can get the task list"""
     #     todos = Todo.objects.all()
-    #     response  = self.client.get(self.url,self.todo-data,format="json")
+    #     response  = self.client.get(self.url)
+    #     self.assertEqual(200,response.status_code)
 
-    # def test_can_get_single_task(self):
-    #     """Test if api can get the task list"""
+    #     serialized_data =TodoSerializer(todos,)
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_task(title="", description="", done=False):
+        if title != "" and description != "":
+            Todo.objects.create(title=title, description=description, done=False)
+
+    def setUp(self):
+        self.create_task("skydive", "go to sagana and skydive")
+        self.create_task("Hike", "hike in the Himalayas")
+        self.create_task("bowling", "go bowling in panari")
+
+
+# test for a list of all tasks
+class GetAllTodo(BaseViewTest):
+
+    def test_get_all_tasks(self):
+        response = self.client.get(
+            reverse('todos')
+        )
+        expected = Todo.objects.all()
+        serialized = TodoSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingletask(BaseViewTest):
+
+    def get_task(self, pk):
+        self.client.get(
+            "todo", kwargs={
+                "pk": pk
+            }
+        )
+
+    def test_get_single(self):
+
+        response = self.get_task(1)
+        task = Todo.objects.get(pk=1)
+        expected = task.title
+        self.assertEqual(expected, "skydive")
+        self.assertEqual(200,response.status_code)
+
+
+
 
